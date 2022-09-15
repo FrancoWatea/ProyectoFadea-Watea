fa3ece76112f04213d5ff1927ddd2129988b4d7c
“FrancoWatea” <franco@watea.com.ar>
2022-09-15 17:58:57 +0200

    defino rutas y queries. elimino del aplication.xml el jdbc:inizialize-databse

--- requisitions/src/main/java/com/requisitions/router/EmployeeRouter.java
+++ requisitions/src/main/java/com/requisitions/router/EmployeeRouter.java
@@ -7,8 +7,16 @@ public class EmployeeRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
     	
-        from("direct:insert").log("Inserted new Employee").bean("employeeMapper", "getMap")
-            .to("sqlComponent:{{sql.insertDemo}}");
+        from("direct:insert").bean("employeeMapper", "getMap")
+            .to("sqlComponent:{{sql.insertDemo1}}").log("Inserted1")
+            .to("sqlComponent:{{sql.insertDemo2}}").log("Inserted2")
+            .to("sqlComponent:{{sql.insertDemo3}}").log("Inserted3")
+            .to("sqlComponent:{{sql.insertDemo3-2}}").log("Inserted4")
+            .to("sqlComponent:{{sql.insertDemo4}}").log("Inserted5")
+            .to("sqlComponent:{{sql.insertDemo4-2}}").log("Inserted6")
+            .to("sqlComponent:{{sql.insertDemo4-3}}").log("Inserted7")
+            .to("sqlComponent:{{sql.insertDemo5}}").log("Inserted8");
+        
 
         from("direct:select").to("sqlComponent:{{sql.selectDemo}}")
             .bean("employeeMapper", "readEmployees").log("${body}");
--- requisitions/src/main/resources/db-schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE DEMO (
-  emp_name VARCHAR(45),
-  emp_age VARCHAR(45)
-);
